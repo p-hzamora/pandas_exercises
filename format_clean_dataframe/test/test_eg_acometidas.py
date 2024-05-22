95a8f4437094c5feb74cc0304c5b0e23d4a6fae3
p-hzamora <p.hzamora@icloud.com>
2024-05-22 17:33:16 +0200

    test: updated test_eg_acometidas.py

--- format_clean_dataframe/test/test_eg_acometidas.py
+++ format_clean_dataframe/test/test_eg_acometidas.py
@@ -6,8 +6,9 @@ sys.path.append(str(Path(__file__).parent.parent))
 
 from src.exercises.acometidas.dataframe import AcometidasDataFrameCreator  # noqa: E402
 
-ruta= r"C:\Users\phzam\pandas_exercises\format_clean_dataframe\src\exercises\acometidas\data.csv"
+ruta= Path(__file__).parent/"data.csv"
 
 print(pd.read_csv(ruta,delimiter=";"))
 df = AcometidasDataFrameCreator().create(ruta)
+print(df)
 df.to_excel("evolucion_obras.xlsx",index=False)
