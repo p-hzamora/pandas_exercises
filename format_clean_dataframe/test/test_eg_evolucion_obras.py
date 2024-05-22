3d3cff8822cf25aa69aab887e932441ce0cdbb98
p-hzamora <p.hzamora@icloud.com>
2024-05-22 18:13:09 +0200

    test: update evolucion_obras test

--- format_clean_dataframe/test/test_eg_evolucion_obras.py
+++ format_clean_dataframe/test/test_eg_evolucion_obras.py
@@ -7,8 +7,10 @@ sys.path.append(str(Path(__file__).parent.parent))
 from src.exercises.evolucion_obras.dataframe import EvolucionObrasDataFrameCreator  # noqa: E402
 
 
-ruta= r"C:\Users\phzam\pandas_exercises\format_clean_dataframe\src\exercises\evolucion_obras\datos_planificacion.csv"
+ruta= Path(__file__).parent/"datos_planificacion.csv"
+
 
 print(pd.read_csv(ruta,delimiter=";"))
 df = EvolucionObrasDataFrameCreator().create(ruta)
+print(df)
 df.to_excel("evolucion_obras.xlsx",index=False)
